9f40b203f48a983b5fd64886321b976665a6bb65
Cullen D <cullen.dolan@gmail.com>
2020-10-17 09:44:43 -0500

    clarified the web page message

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn how to use React
           </a>
         </header>
       </div>
